58ea6cf6e2fc5fc8dda3623e30a215882e0a7854
João <joaopedrolima.s@hotmail.com>
2020-05-23 22:41:47 -0300

    Stack e positioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/pages/home_page/home_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       
       ),
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
